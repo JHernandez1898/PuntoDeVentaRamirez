a246eb26e11f4b283924909ffa61eecfc274ccd8
Pablo Lavín <thepabloface@hotmail.com>
2018-11-30 12:15:21 -0600

    Creación de la clase de conexión a reportes. Preparación del método consultar ventas por fechas. Creación del DataSet preparado para dicho método.

--- PuntoDeVentaRamirez/ClasesBD/ConexionBD.cs
+++ PuntoDeVentaRamirez/ClasesBD/ConexionBD.cs
@@ -9,9 +9,11 @@ namespace PuntoDeVentaRamirez
 {
     public class ConexionBD
     {
+
         public static SqlConnection ObtenerConexion()
         {
-            string strDataSource = @"DESKTOP-8I9VLRK\SQLEXPRESS";
+            //string strDataSource = @"DESKTOP-8I9VLRK\SQLEXPRESS";
+            string strDataSource = @"LA-DIVERTIDA\SQLEXPRESS";
             string strCatolog = "ELECTRONICA";
             SqlConnection con = new SqlConnection(string.Format("Data Source={0};Initial Catalog={1};Integrated Security=True",strDataSource,strCatolog));
             con.Open();
---
+++ PuntoDeVentaRamirez/ClasesBD/ConexionesAReportes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using PuntoDeVentaRamirez.Reportes;
+
+namespace PuntoDeVentaRamirez.ClasesBD
+{
+    class ConexionesAReportes
+    {
+        public static DataTable ConsultarVentasPorFechas(DateTime fechaInicial, DateTime fechaFinal)
+        {
+            DatosVentas datosVentas = new DatosVentas();
+            using (SqlConnection con = ConexionBD.ObtenerConexion())
+            {
+                try
+                {
+                    SqlCommand comando = new SqlCommand(@"exec Reporte_Ventas '"+fechaInicial.ToString("yy-mm-dd")+@"','"+fechaFinal.ToString("yy-mm-dd") + @"'", con);
+                    SqlDataAdapter adaptador = new SqlDataAdapter();
+                    adaptador.SelectCommand = comando;
+                    adaptador.Fill(datosVentas);                    
+                }
+                catch (SqlException e)
+                {
+                    throw new Exception(e.Message);
+                }
+            }
+            return datosVentas.Tables[0];
+        }
+    }
+}
